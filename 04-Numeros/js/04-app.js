5e81a77e2c082991cd559f2522a648becc4042e4
Brayan8abo <brayan8abo@outlook.es>
2024-11-29 23:24:06 +0100

    update

--- 04-Numeros/js/04-app.js
+++ 04-Numeros/js/04-app.js
@@ -5,6 +5,11 @@ resultado = 20 + 30 * 2;
 resultado = (20+30) *2;
 
 //20% descuento
-resultado =  20+30+40+50+60 *.2 ;
+resultado =  (20+30+40+50+60) *.2 ;
+
+
+resultado = (20+30) * 1.16; //seria 16% 
+
+ 
 
 console.log(resultado);
